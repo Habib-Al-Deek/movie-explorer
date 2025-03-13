631b33098603a284bfaae16691ba64881d7d8dc5
Habib Al Deek <deek_habib@outlook.com>
2025-03-13 09:34:56 +0200

    updated api url

--- app/api/movieApi.js
+++ app/api/movieApi.js
@@ -12,7 +12,7 @@ export async function fetchMovies() {
   try {
     // This calls TMDB's "discover" endpoint
     const response = await fetch(
-      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+      "https://api.themoviedb.org/3/discover/movie?&page=1",
       options
     );
 
